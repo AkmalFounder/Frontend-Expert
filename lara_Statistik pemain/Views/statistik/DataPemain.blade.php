9a5253eb6d991a5a6f5037875325013ca75b2c49
akmalikhsan <63221748+AkmalFounder@users.noreply.github.com>
2022-02-03 23:29:22 +0700

    view

---
+++ lara_Statistik pemain/Views/statistik/DataPemain.blade.php
@@ -0,0 +1,277 @@
+@extends('layouts.home')
+
+@section('content')
+
+<!-- Content Header (Page header) -->
+<section class="content-header">
+     <h1>
+       Players
+       <small>containing players statistics</small>
+     </h1>
+     <ol class="breadcrumb">
+       <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+       <li><a href="#">Players</a></li>
+       <li class="active">Top 5</li>
+     </ol>
+   </section>
+
+   <!-- Main content -->
+   <section class="content">
+
+     <!-- Default box -->
+    
+     <div class="col-md-6">
+          <div class="box">
+            <div class="box-body">
+              <h3 class="box-title"><img src="https://fifastatic.fifaindex.com/FIFA22/players/246430.png"><img src="https://fifastatic.fifaindex.com/FIFA21/images/flags/2/51.png">Serbia</h3></img></img>
+
+           
+            </div>
+            <!-- /.box-header -->
+            <div class="table-responsive">
+              <table class="table no-margin">
+             
+                <tr>
+                  <th>Dušan Vlahović</th>
+                  <th><span class="label label-success">80</span> <span class="label label-success">86</span></th>
+                </tr>
+                <tr>
+                  <td>Name</td>
+                  <td>Dušan Vlahović</td>
+                </tr>
+                <tr>
+                  <td>Rating</td>
+                  <td>85</td>
+                  <td>
+                </tr>
+                <tr>
+                  <td>Age</td>
+                  <td>22</td>
+                  <td>
+                </tr>
+                <tr>
+                  <td>Height</td>
+                  <td>186</td>
+                  <td>
+                </tr>
+                <tr>
+                  <td>Weight</td>
+                  <td>75</td>
+                  <td>
+                </tr>
+                <tr>
+                  <td>Country</td>
+                  <td>Germany</td>
+                  <td>
+                </tr>
+                <tr>
+                  <td>TTL</td>
+                  <td>Feb. 5, 1985</td>
+                  <td>
+                </tr>
+                <tr>
+                  <td>Preferred Foot</td>
+                  <td>Right</td>
+                  <td>
+                </tr>
+                <tr>
+                  <td>Weak Foot</td>
+                  <td>4</td>
+                  <td>
+                </tr>
+                <tr>
+                  <td>Value</td>
+                  <td>€45.000.000s</td>
+                  <td>
+                </tr>
+              </table>
+            </div>
+          </div>     
+        </div>
+        <!-- /.box pemain  -->
+
+        <div class="col-md-4">
+          <div class="box">
+            <div class="box-body">
+           
+            </div>
+            <!-- /.box-header -->
+            <div class="table-responsive">
+              <table class="table no-margin">
+                <tr>
+                  <th>Ball Skill</th>
+                  <!-- <th><span class="label label-success">80</span> <span class="label label-success">86</span></th> -->
+                </tr>
+                <tr>
+                  <td>Ball Control</td>
+                  <td>88</td>
+                </tr>
+                <tr>
+                  <td>Dribling</td>
+                  <td>89</td>
+                  <td>
+                </tr>
+                <tr>
+              </table>
+            </div>
+          </div>     
+        </div>
+        <!-- /.box BallSkill  -->
+                
+       <div class="col-md-4">
+          <div class="box">
+            <div class="box-body">
+           
+            </div>
+            <!-- /.box-header -->
+            <div class="table-responsive">
+              <table class="table no-margin">
+                <tr>
+                  <th>Passing</th>
+                  <!-- <th><span class="label label-success">80</span> <span class="label label-success">86</span></th> -->
+                </tr>
+                <tr>
+                  <td>Crossing</td>
+                  <td>67</td>
+                </tr>
+                <tr>
+                  <td>Short pass</td>
+                  <td>77</td>
+                  <td>
+                </tr>
+                <tr>
+                  <td>Long pass</td>
+                  <td>70</td>
+                  <td>
+                </tr>
+              </table>
+            </div>
+          </div>     
+        </div>
+     <!-- /.box Passing  -->
+     <div class="col-md-4">
+          <div class="box">
+            <div class="box-body">
+           
+            </div>
+            <!-- /.box-header -->
+            <div class="table-responsive">
+              <table class="table no-margin">
+                <tr>
+                  <th>Defence</th>
+                  <!-- <th><span class="label label-success">80</span> <span class="label label-success">86</span></th> -->
+                </tr>
+                <tr>
+                  <td>Marking</td>
+                  <td>34</td>
+                </tr>
+                <tr>
+                  <td>Tackle</td>
+                  <td>24</td>
+                  <td>
+                </tr>
+              </table>
+            </div>
+          </div>     
+        </div>
+    <!-- /.box Defence  -->
+    <div class="col-md-4">
+          <div class="box">
+            <div class="box-body">
+           
+            </div>
+            <!-- /.box-header -->
+            <div class="table-responsive">
+              <table class="table no-margin">
+                <tr>
+                  <th>Shooting</th>
+                  <!-- <th><span class="label label-success">80</span> <span class="label label-success">86</span></th> -->
+                </tr>
+                <tr>
+                  <td>Heading</td>
+                  <td>90</td>
+                </tr>
+                <tr>
+                  <td>Finishing</td>
+                  <td>94</td>
+                  <td>
+                </tr>
+                <tr>
+                  <td>Penalties</td>
+                  <td>95</td>
+                  <td>
+                </tr>
+              </table>
+            </div>
+          </div>     
+        </div>
+      <!-- /.box Shooting  -->
+
+      <div class="col-md-5">
+          <div class="box">
+            <div class="box-body">
+           
+            </div>
+            <!-- /.box-header -->
+            <div class="table-body no-padding">
+              <table class="table no-margin">
+                <tr>
+                  <th>Physical</th>
+                  <!-- <th><span class="label label-success">80</span> <span class="label label-success">86</span></th> -->
+                </tr>
+                <tr>
+                  <td>Acceleration</td>
+                  <td>78</td>
+                </tr>
+                <tr>
+                  <td>Stamina</td>
+                  <td>84</td>
+                  <td>
+                </tr>
+                <tr>
+                  <td>Speed</td>
+                  <td>94</td>
+                  <td>
+                </tr>
+              </table>
+            </div>
+          </div>     
+        </div>
+         <!-- /.box Physical  -->
+        
+         <div class="col-md-5">
+          <div class="box">
+            <div class="box-body">
+           
+            </div>
+            <!-- /.box-header -->
+            <div class="table-body no-padding">
+              <table class="table no-margin">
+                <tr>
+                  <th>Goal Keeper</th>
+                  <!-- <th><span class="label label-success">80</span> <span class="label label-success">86</span></th> -->
+                </tr>
+                <tr>
+                  <td>Reflexes</td>
+                  <td>11</td>
+                </tr>
+                <tr>
+                  <td>Positioning</td>
+                  <td>9</td>
+                  <td>
+                </tr>
+                <tr>
+                  <td>Handling</td>
+                  <td>11</td>
+                  <td>
+                </tr>
+              </table>
+            </div>
+          </div>     
+        </div>
+
+        <!-- /.box Goal Keeper  -->
+    <!-- /.box-footer-->
+   </section>
+   <!-- /.content -->
+   @endsection
---
+++ lara_Statistik pemain/Views/statistik/pemain.blade.php
@@ -0,0 +1,117 @@
+@extends('layouts.home')
+
+@section('content')
+
+<!-- Content Header (Page header) -->
+<section class="content-header">
+     <h1>
+       Players
+       <small>containing players statistics</small>
+     </h1>
+     <ol class="breadcrumb">
+       <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+       <li><a href="#">Players</a></li>
+       <li class="active">Top 5</li>
+     </ol>
+   </section>
+
+   <!-- Main content -->
+   <section class="content">
+
+     <!-- Default box -->
+    
+       <div class="box-body">
+       <!--content-->
+             <!-- TABLE: LATEST ORDERS -->
+            <div class="box box-info">
+            <div class="box-header with-border">
+              <h3 class="box-title">Top 5</h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <div class="table-responsive">
+                <table class="table no-margin">
+                  <thead>
+                  @php 
+                    $no = 1;
+                  @endphp
+                  @foreach($data as $index => $row)
+                  <tr>
+                    <th scope="row">{{ $row->id }}</th>
+                    <!-- <th></th> -->
+                    <th>Name</th>
+                    <th>Positions</th>
+                    <th>Age</th>
+                    <th>update</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  
+                  <tr>
+                    <td></td>
+                    <!-- <td><img src="https://fifastatic.fifaindex.com/FIFA22/players/246430.png"></img></td> -->
+                    <td><a style="color:black" href="/Datapemain"><span>{{ $row->name }}</span></td></a>
+                    <td><span class="label label-info">{{ $row->position }}</span></td>
+                    <td>
+                      <div class="sparkbar">{{ $row->age }}</div>
+                    </td>
+                    <td>
+                    <a href="/ubahdata/{{$row->id}}"  class="btn btn-info">Edit</a>
+                    </td>
+                  </tr>
+                  <!-- <tr>
+                    <td><img src="https://fifastatic.fifaindex.com/FIFA22/players/223033.png"></img></td>
+                    <td>{{ $row->name }}</td>
+                    <td><span class="label label-warning">{{ $row->position }}</span></td>
+                    <td>
+                      <div class="sparkbar">{{ $row->age }}</div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td><img src="https://fifastatic.fifaindex.com/FIFA22/players/239085.png"></img></td>
+                    <td>Erling Haaland</td>
+                    <td><span class="label label-info">ST</span></td>
+                    <td>
+                      <div class="sparkbar" data-color="#f56954" data-height="20">21</div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td><img src="https://fifastatic.fifaindex.com/FIFA22/players/238794.png"></img></td>
+                    <td>Vinícius Jr.</td>
+                    <td><span class="label label-info">LW</span></td>
+                    <td>
+                      <div class="sparkbar">21</div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td><img src="https://fifastatic.fifaindex.com/FIFA22/players/255475.png"></img></td>
+                    <td>Antony</td>
+                    <td><span class="label label-info">RW</span></td>
+                    <td>
+                      <div class="sparkbar" data-color="#f39c12" data-height="20">21</div>
+                    </td>
+                  </tr> -->
+                  @endforeach
+                  </tbody>
+                </table>
+                
+              </div>
+              <!-- /.table-responsive -->
+            </div>
+         
+          </div>
+          <!-- /.box -->
+        </div>
+
+       <!-- /.box-footer-->
+  
+
+   </section>
+   <!-- /.content -->
+   @endsection
